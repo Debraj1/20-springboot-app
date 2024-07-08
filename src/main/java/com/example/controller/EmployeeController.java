a496ea53b131d67b84d7c92151ca3623d46df79c
Debraj <debrajd1983@gmail.com>
2024-07-08 15:27:06 +0530

    asd

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -2,12 +2,16 @@ package com.example.controller;
 
 import com.example.model.Employee;
 import com.example.repository.EmployeeRepository;
+import com.example.service.KafkaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/employee")
@@ -18,6 +22,8 @@ public class EmployeeController {
 
     @Autowired
     EmployeeRepository repository;
+    @Autowired
+    private KafkaService kafkaService;
 
     @PostMapping
     public Employee add(@RequestBody Employee employee) {
@@ -41,6 +47,18 @@ public class EmployeeController {
     public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
         LOGGER.info("Employee find: departmentId={}", departmentId);
         return repository.findByDepartment(departmentId);
+    }
+
+    @GetMapping("/dog")
+    public String getDogName(){
+        return  "Lalu";
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateLocation(){
+
+        kafkaService.updateLocation("Kolkata");
+        return new ResponseEntity<>(Map.of("message","Location Updated"), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import com.example.exception.UserNotFoundException;
+import com.example.model.User;
+import com.example.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+@RestController
+public class UserController
+{
+    @Autowired
+    private UserDaoService service;
+    @GetMapping("/users")
+    public List<User> retriveAllUsers()
+    {
+        return service.findAll();
+    }
+    //retrieves a specific user detail
+    @GetMapping("/users/{id}")
+    public User retriveUser(@PathVariable int id)
+    {
+        User user= service.findOne(id);
+        if(user==null)
+//runtime exception
+            throw new UserNotFoundException("id: "+ id);
+        return user;
+    }
+    //method that posts a new user detail and returns the status of the user resource
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user)
+    {
+        User sevedUser=service.save(user);
+        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(sevedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
